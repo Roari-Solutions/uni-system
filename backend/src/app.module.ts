c7082fbc2f8b119c7f19a270fffb618d93fcd8e3
oovaa <omarvx211@gmail.com>
2026-08-23 19:29:35 +0200

    feat: init dynamic content resourse at 2026-08-23

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import { ContentModule } from './content/content.module';
 
 @Module({
   imports: [
@@ -13,6 +14,8 @@ import { ConfigModule } from '@nestjs/config';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+
+    ContentModule,
   ],
   controllers: [AppController],
   providers: [AppService],
