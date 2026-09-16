568c3e967f2c470b5849061a6a0ffe6ed3e2c8fc
A1iHassan <ali012wkout@gmail.com>
2026-09-16 20:59:29 +0300

    chore(web):clean out old code from repo

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,14 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router'
 import "./i18n";
-import MainLayout from './layouts/mainLayout'
 import './index.css'
 import MainPage from './pages/mainPage'
 
 const router = createBrowserRouter([
 	{
 		path: "/",
-		element: <MainLayout />,
+		element: <>Hi</>,
 		children: [
 			{ index: true, element: <MainPage />},
 		],
