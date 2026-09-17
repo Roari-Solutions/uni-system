c7d9400c9632bd2368504f7e7747ff8dcf57ade2
Omar <omarvx211@gmail.com>
2026-09-17 22:00:13 +0200

    feat(grades): CRUD for grades with faculty scoping

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,6 +5,7 @@ import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { ContentModule } from './content/content.module';
+import { GradesModule } from './grades/grades.module';
 import { StudentsModule } from './students/students.module';
 import { CurriculumsModule } from './curriculums/curriculums.module';
 import { GrGurdGuard } from './gr-gurd/gr-gurd.guard';
@@ -16,6 +17,7 @@ import { GrGurdGuard } from './gr-gurd/gr-gurd.guard';
     }),
 
     ContentModule,
+    GradesModule,
     StudentsModule,
     CurriculumsModule,
     AuthModule,
@@ -24,6 +26,5 @@ import { GrGurdGuard } from './gr-gurd/gr-gurd.guard';
   controllers: [AppController],
   providers: [AppService, GrGurdGuard],
 })
-
 /** Root module: global config plus auth/content/database. */
 export class AppModule {}
