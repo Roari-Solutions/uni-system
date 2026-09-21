ddbbe4697211f218d911a23630d1fdbbfcbe23d9
Omar <omarvx211@gmail.com>
2026-09-21 20:34:18 +0200

    chore: port the types from json to ts entity types

---
+++ backend/src/content/entities/partnerships.entity.ts
@@ -0,0 +1,28 @@
+export type Partnerships = {
+  heroSection: {
+    mainHead: string;
+    subHead: string;
+    partnershipVision: string;
+  };
+  stats: {
+    icon: string;
+    title: string;
+    content: string;
+  }[];
+  partnerships: {
+    icon: string;
+    tag: string;
+    title: string;
+    subTitle: string;
+    content: string;
+    field: string;
+    date: string;
+  }[];
+  contactsCard: {
+    heading: string;
+    subHeading: string;
+    email: string;
+    phone: string;
+    location: string;
+  };
+};
